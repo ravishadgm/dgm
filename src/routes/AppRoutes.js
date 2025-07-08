79df0bf8030d5fd59a3fac6bddcfbc95c53a5a38
ravishadgm <ravishapatel.dgm@gmail.com>
2025-07-08 18:44:51 +0530

    project setup

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+     
+    </Routes>
+  );
+};
+
+export default AppRoutes;
